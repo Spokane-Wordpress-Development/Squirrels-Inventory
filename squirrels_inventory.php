55e45bf54b01bd26c064df5b456a91f0d141bcab
Tony DeStefano <spokanetony@gmail.com>
2015-12-14 17:16:17 -0800

    Initial commit

---
+++ squirrels_inventory.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Squirrels Inventory
+ * Plugin URI: http://www.ibrake4squirrels.com
+ * Description: Auto Inventory Plug-In
+ * Version: 1.0.0
+ * Author: Tony DeStefano, Ethan Federman
+ * Text Domain:
+ * Domain Path:
+ * Network:
+ * License: GPL2
+ */
