fce66799a6149293d8ed9b852db843658574f961
Tony DeStefano <spokanetony@gmail.com>
2016-02-24 10:03:36 -0800

    Typo

--- squirrels_inventory.php
+++ squirrels_inventory.php
@@ -24,7 +24,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * @package CaptainCountdown
+ * @package SquirrelsInventory
  * @author Spokane WordPress Development
  * @version 1.0.0
  */
